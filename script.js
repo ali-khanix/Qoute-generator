348c08eb6320e2311c07834e3bc71ccd78807ba1
Ali Khani <khaniali568@gmail.com>
2023-02-11 09:57:56 +0330

    Fetching data and show on screen

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('Hello world');
+let apiQuotes = [];
+
+const quoteContainer = document.getElementById('quote-container');
+
+const textQuote = document.getElementById('quote');
+const textAuthor = document.getElementById('author');
+
+const btnTwitter = document.getElementById('twitter-btn');
+const btnNewQuote = document.getElementById('new-quote-btn');
+
+// Show new quote
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  textQuote.textContent = quote.text;
+  author.textContent = quote.author;
+}
+
+btnNewQuote.addEventListener('click', () => {});
+
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //Cath error here
+  }
+}
+
+// On Load
+getQuotes();
